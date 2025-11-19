155bc7eae4da52796ff25338a5b910173265f62d
PhoenixEmik <phoenix0919mik@gmail.com>
2025-11-19 16:19:15 +0800

    Refactor make_module_extra to set shell functions for aliases in Apptainer

--- easybuild/easyblocks/generic/apptainer.py
+++ easybuild/easyblocks/generic/apptainer.py
@@ -124,9 +124,9 @@ class Apptainer(Binary):
         txt = super(Apptainer, self).make_module_extra(*args, **kwargs)
         for alias in self.cfg["aliases"]:
             bash_function = "apptainer exec %s %s %s \"$@\"" % (self.cfg["apptainer_params"], self.cfg["container_path"], alias)
-        #    csh_function = "apptainer exec %s %s %s $*" % (self.cfg["apptainer_params"], self.cfg["container_path"], alias)
+            csh_function = "apptainer exec %s %s %s $*" % (self.cfg["apptainer_params"], self.cfg["container_path"], alias)
         #    txt += self.module_generator.set_shell_function(alias, bash_function, csh_function)
-            txt += bash_function + "\n"
+            txt += "set_shell_function(%s, %s, %s)\n" % (alias, bash_function, csh_function)
         return txt
 
     def sanity_check_step(self):
